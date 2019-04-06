2a2871d2f946fa05d3063e1957461e04242ade32
XenoPlat <43900946+XenoPlat@users.noreply.github.com>
2019-04-06 08:05:18 +0300

    ДЗ_4
    Решения к уроку 4

---
+++ dz_4.py
@@ -0,0 +1,40 @@
+# Написать два алгоритма нахождения i-го по счёту простого числа.
+
+# 1)
+
+def primes(n):
+    a = [0] * 100
+    for i in range(n): 
+        a[i] = i      
+    
+    a[1] = 0
+     
+    m = 2 
+    while m < n: 
+        if a[m] != 0: 
+            j = m * 2 
+            while j < n:
+                a[j] = 0 
+                j = j + m 
+        m += 1
+         
+    b = []
+    for i in a:
+        if a[i] != 0:
+            b.append(a[i])
+     
+    del a
+    return b
+ 
+# 2)
+
+def primes(n):
+  m = set(range(2, n))
+  for i in range(2, int(math.sqrt(n))):
+    if i in m:
+      m -= set(range(2*i, n, i))
+  return m
+
+print primes(10)
+
+
